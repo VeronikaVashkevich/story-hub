ab4d4f894a616934ecf30f2d12cfe023678ca2c3
Veronika Vashkevich <veronik.vashkevich@gmail.com>
2025-06-01 18:59:36 +0300

    Added Tag model

--- app/Models/Story.php
+++ app/Models/Story.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Story extends Model
 {
@@ -21,5 +22,10 @@ class Story extends Model
     public function fandom(): BelongsTo
     {
         return $this->belongsTo(Fandom::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }
