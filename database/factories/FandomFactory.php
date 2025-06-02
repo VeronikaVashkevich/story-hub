15d29cebcccbd7ac23848b1b092f5c2ea85b371a
Veronika Vashkevich <veronik.vashkevich@gmail.com>
2025-06-02 20:40:04 +0300

    Fandom factory

---
+++ database/factories/FandomFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Fandom>
+ */
+class FandomFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => ucfirst(fake()->words(3, true)),
+            'description' => fake()->paragraphs(2, true),
+        ];
+    }
+}
